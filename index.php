b198d6072052c8867a108be90a0efd996f41aa12
Collin Dobmeyer <cdobmeyer@smu.edu>
2012-12-01 15:10:50 -0600

    resize logo image

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 <body>
 	<?php menubar(); ?>
 	<div>
-	<img src="images/gui/smalllogo.png" alt="picnit.net" height="150" id="logo">
+	<img src="images/gui/smalllogo.png" alt="picnit.net" height="100" id="logo">
 	</div>
 	<?php searchbar(); ?>
 	<div id="gallery" name="gallery" class="panels">
